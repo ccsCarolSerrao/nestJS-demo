e70f866c68beca20709fe1da2a9a87464be5eda9
Carolina Serrao | Stone <carolina.serrao@stone.com.br>
2020-03-23 02:46:50 -0300

    feat: start code guards

--- src/example-api/v1/example-api.controller.ts
+++ src/example-api/v1/example-api.controller.ts
@@ -1,36 +1,45 @@
-import { Controller, Get, Post, Body, Query, Param, Put, Delete, HttpCode } from '@nestjs/common'
+import { Controller, Get, Post, Body, Query, Param, Put, Delete, HttpCode, UseGuards } from '@nestjs/common'
 
 import ExampleApiFilterDto from './dtos/example-api-filter.dto'
 import ExampleApiCreateDto from './dtos/example-api-create.dto'
 import ExampleApiUpdateDto from './dtos/example-api-update.dto'
 import ExampleApiService from './example-api.service'
+import { RolesGuard } from 'guards/role.guard'
+import { Roles } from 'decorators/role.decorator'
+
 
 @Controller('v1/example-api')
+@UseGuards(RolesGuard)
 export default class ExampleApiController {
     constructor(private exampleService: ExampleApiService) {}
 
     @Post()
     @HttpCode(201)
+    @Roles('example:create')
     async create(@Body() exampleApi: ExampleApiCreateDto) {
         return await this.exampleService.create(exampleApi)
     }
 
     @Get()
+    @Roles('example:search')
     async findAll(@Query() query: ExampleApiFilterDto) {
         return await this.exampleService.findAll(query)
     }
 
     @Get(':id')
+    @Roles('example:get')
     async findOne(@Param('id') id: string) {
         return await this.exampleService.findOne(id)
     }
 
     @Put(':id')
+    @Roles('example:update')
     async update(@Param('id') id: string, @Body() exampleApi: ExampleApiUpdateDto) {
         return await this.exampleService.update(exampleApi, id)
     }
 
     @Delete(':id')
+    @Roles('example:remove')
     async remove(@Param('id') id: string) {
         return await this.exampleService.delete(id)
     }
--- src/example-api/v1/example-api.module.ts
+++ src/example-api/v1/example-api.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common'
 import ExampleApiController from './example-api.controller'
 import ExampleApiService from './example-api.service'
+import { RolesGuard } from 'guards/role.guard'
+import { AuthGuard } from 'guards/auth.guard'
 
 @Module({
     controllers: [ExampleApiController],
-    providers: [ExampleApiService],
+    providers: [AuthGuard, RolesGuard, ExampleApiService],
 })
 export default class ExampleApiModule {}
